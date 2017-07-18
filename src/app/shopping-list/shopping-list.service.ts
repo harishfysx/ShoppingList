d16f23c8c92d3c1a4b24157fc87d96214fb8eda6
harishfysx <harishfysx@gmail.com>
2017-07-18 15:56:32 -0500

    Implmented Service in Shopping List - Lect108

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
+import {Ingredient} from "../shared/ingredients.model";
+import {EventEmitter} from "@angular/core";
 export class ShoppingListService{
+  ingredientChanged = new EventEmitter<Ingredient[]>();
 
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apple',5),
+    new Ingredient('Mango',10)
+  ];
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient : Ingredient){
+    this.ingredients.push(ingredient);
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
 }
