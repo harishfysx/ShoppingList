bae9ae614601b043835511f78a71b6614aef1e93
harishfysx <harishfysx@gmail.com>
2017-07-22 20:05:57 -0500

    Added Routes- Lect143

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from "./shared/dropdown.directive";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
@@ -29,7 +30,10 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
     DropdownDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
